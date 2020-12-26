a42cd5054ae020b014e2c07a52e5db22be00f504
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2020-12-26 20:32:21 +0100

    changes in navbar

--- src/components/Navbar/Navitem.jsx
+++ src/components/Navbar/Navitem.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 class Navitem extends Component {
   render() {
     return (
       <li id={this.props.item}>
-        <Link
+        <NavLink
+          exact
+          activeStyle={{color:'#fa923f'}}
           to={this.props.tolink}
           onClick={this.props.activec.bind(this, this.props.item)}
         >
           {this.props.item}
-        </Link>
+        </NavLink>
       </li>
     );
   }
