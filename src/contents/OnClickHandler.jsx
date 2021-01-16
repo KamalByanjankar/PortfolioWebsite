52f77c29ffecb62289c03e70eafd148abe347126
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2021-01-16 14:32:10 +0100

    Sidebar close

--- src/contents/OnClickHandler.jsx
+++ src/contents/OnClickHandler.jsx
@@ -9,7 +9,7 @@ const ClickHandler = () => {
 
     const drawerToggleClickHandler = () => (
         setSideDrawerOpen(prevState => {
-            return(!prevState.sideDrawerOpen)
+            return(!prevState)
             
         })
     )    
