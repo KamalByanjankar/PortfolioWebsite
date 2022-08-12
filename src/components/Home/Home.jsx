97cf7ce605ce87eb60641f167a0b96dde1721451
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2022-08-12 11:50:49 +0200

    image name refactored and home component edited

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 // import ReactTypingEffect from "react-typing-effect";
+import homeImage from "../../assets/profile/kamal_home.jpg";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="header__image">
+      <img src={homeImage} alt="home" />
       <div className="home__container">
         <h1>Kamal Byanjankar</h1>
         <p>Software Engineer</p>
