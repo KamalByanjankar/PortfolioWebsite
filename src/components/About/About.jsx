f0048f1029e43acc5d388d2d3c4b7c53cf3257ba
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2022-11-03 09:38:40 +0100

    spelling corrected

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,32 +4,32 @@ import image from "../../assets/profile/kamal.png"
 import "./About.css"
 
 const About = () => {
-  return(
+  return (
     <div className="header about">
       <h1>About</h1>
       <div className="about__content">
         <div className="about__image">
-          <img src={image} alt="self"/>
+          <img src={image} alt="self" />
         </div>
         <div className="about__linespace">
-          Hi! My name is Kamal Byanjankar and I am from Nepal. I did my Master in Engineering in Information Technology from 
-          Frankfurt University of Applied Scienes. I completed my Master study with a final thesis on  
-          <b> Recognition of Small Stone Obstacles for Autonomous Driving</b> using Machine Learning algorithms. I did 
-          my Bachelor in Engineering in Electronics and Communication Engineering from Kantipur Engineering College in Nepal with a 
+          Hi! My name is Kamal Byanjankar and I am from Nepal. I did my Master in Engineering in Information Technology from
+          Frankfurt University of Applied Sciences. I completed my Master study with a final thesis on
+          <b> Recognition of Small Stone Obstacles for Autonomous Driving</b> using Machine Learning algorithms. I did
+          my Bachelor in Engineering in Electronics and Communication Engineering from Kantipur Engineering College in Nepal with a
           final year project on <b>Automatic Solar Charger with Tracking</b>.
-          <br/>
-          <br/>
-          Currently, I am doing projects on Reactjs and Java Spring boot. During my Master's, as a part of curriculum projects, I have 
-          done projects using Java, python, and C# programming languages. As learning is a never ending process, I have been doing 
-          projects to improve my knowledge and skills. All the projects are available in <a 
-            href="https://github.com/kamalByanjankar" 
+          <br />
+          <br />
+          Currently, I am doing projects on Reactjs and Java Spring boot. During my Master's, as a part of curriculum projects, I have
+          done projects using Java, python, and C# programming languages. As learning is a never ending process, I have been doing
+          projects to improve my knowledge and skills. All the projects are available in <a
+            href="https://github.com/kamalByanjankar"
             target="_blank"
-            rel="noopener noreferrer"> 
-             Github
+            rel="noopener noreferrer">
+            Github
           </a>.
-          <br/>
-          <br/>
-          I am a motivated and open minded person, trying to achieve my goals. Talking about my hobbies, I like to read, 
+          <br />
+          <br />
+          I am a motivated and open minded person, trying to achieve my goals. Talking about my hobbies, I like to read,
           listen music, and travelling.
         </div>
       </div>
